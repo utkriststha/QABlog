2c4e3bb2eed0ae8d694b22b2b83d6692891c8244
utkristshrestha <utkrist.shrestha12@gmail.com>
2022-07-27 18:27:19 +1000

    Updates

---
+++ precache-manifest.86ae8c940179da26f652a3621c83dd3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc4dd96242ec37ddce67",
+    "url": "/QABlog/static/css/main.cf611600.chunk.css"
+  },
+  {
+    "revision": "cc4dd96242ec37ddce67",
+    "url": "/QABlog/static/js/main.cc4dd962.chunk.js"
+  },
+  {
+    "revision": "ef720786054a3891a927",
+    "url": "/QABlog/static/js/1.ef720786.chunk.js"
+  },
+  {
+    "revision": "d51ae54b824c38084cd3",
+    "url": "/QABlog/static/js/runtime~main.d51ae54b.js"
+  },
+  {
+    "revision": "ac4141d0657e0fa6000e1c84c7e49571",
+    "url": "/QABlog/index.html"
+  }
+];
